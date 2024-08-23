eb396529d6148104a5fbb73bc1b52f8fb69f96b5
Niraaj10 <nitcode10@gmail.com>
2024-08-23 18:36:23 +0530

    Sign up Page

--- frontend/src/components/Login/signup.js
+++ frontend/src/components/Login/signup.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom';
+import Footer from '../footerr/Footer';
+import { MdKeyboardDoubleArrowRight } from 'react-icons/md';
 
 const Signup = () => {
   const [user, setUser] = useState({
@@ -10,36 +12,38 @@ const Signup = () => {
   });
 
   const onChangeInput = (e) => {
-    const {name,value} = e.target;
-    setUser({...user,[name]:value})
+    const { name, value } = e.target;
+    setUser({ ...user, [name]: value })
   }
 
   const signupSubmit = async (e) => {
     e.preventDefault();
-    
+
     try {
-      await axios.post('/user/signup',{...user})
+      await axios.post('/user/signup', { ...user })
       // console.log({...user})
       // console.log(user)
 
-      localStorage.setItem('Signup User', true)
+      localStorage.setItem('Login User', true)
+      // localStorage.setItem('Signup User', true)
 
       window.location.href = '/'
+      // window.location.href = '/login'
 
 
     } catch (error) {
-        alert(error.response.data.msg)
-        console.log(error)
+      alert(error.response.data.msg)
+      console.log(error)
     }
   }
 
 
   return (
     <>
-      <div>
-      Signupppp
+      <div className='SignUp mt-[140px]'>
+        {/* Signupppp */}
 
-        <form onSubmit={signupSubmit} className='flex flex-col gap-2'>
+        {/* <form onSubmit={signupSubmit} className='flex flex-col gap-2'>
           <input type='text' value={user.name} name='name' placeholder='Name' required onChange={onChangeInput}/>
 
           <input type='email' value={user.email} name='email' placeholder='Email' required onChange={onChangeInput}/>
@@ -54,7 +58,59 @@ const Signup = () => {
           <Link to='/login'>
            Loginnnn
           </Link>
+        </div> */}
+
+        <div className='h-[50vh]  mx-auto flex gap-11 my-7 mt-3 rounded-3xl p-4'>
+
+          <div className='basis-[50%] py-1 px-12'>
+
+            <div className='text-2xl font-bold mb-5'>Signup</div>
+
+            <form onSubmit={signupSubmit} className='flex flex-col gap-5'>
+              <input type='text' value={user.name} name='name' placeholder='Name' required onChange={onChangeInput} className='p-3 rounded-xl mx-11' />
+              <input type='email' value={user.email} name='email' placeholder='Email' required onChange={onChangeInput} className='p-3 rounded-xl mx-11' />
+              <input type='password' value={user.password} name='password' placeholder='Password' required onChange={onChangeInput} className='p-3 rounded-xl mx-11' />
+
+              <button type='submit' className='my-4 p-3 rounded-xl mx-11 bg-black text-white font-bold'>Create new userr</button>
+            </form>
+
+            <div className='mx-12 font-semibold flex flex-col'>
+              already have an Account??
+              <Link to='/login' className='text-[#4A69E2] font-bold'>
+              Login
+              </Link>
+            </div>
+
+          </div>
+
+
+          <div className='bg-white basis-[50%] p-6 px-14 rounded-3xl'>
+
+            <div className='text-3xl font-bold mb-4'>Join CHICKICKS Club Get Rewarded Today.</div>
+
+            <div className='mb-7'>
+              <div>As chickicks club member you get rewarded with what you love for doing what you love.
+                Sign up today and receive immediate access to these Level 1 benefits:
+
+                <ul className='list-disc pl-6 pt-1'>
+                  <li>Free shipping</li>
+                  <li>A 15% off voucher for your next purchase</li>
+                  <li>Access to members Only products and sales</li>
+                  <li>Special offers, Discounts, promotions and many moree</li>
+                </ul>
+              </div>
+            </div>
+
+            <div className='bg-black text-white p-3 rounded-xl flex justify-between items-center'>
+              JOIN THE CLUB
+              <MdKeyboardDoubleArrowRight color='white' size={20} />
+            </div>
+
+          </div>
         </div>
+
+
+        <Footer />
 
       </div>
     </>
