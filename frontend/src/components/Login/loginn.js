fdf01115f64141adb3e0e7a467e369e38e63a647
Niraaj10 <nitcode10@gmail.com>
2024-08-23 18:20:48 +0530

    Login Page

--- frontend/src/components/Login/loginn.js
+++ frontend/src/components/Login/loginn.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom';
+import Footer from '../footerr/Footer';
+import { MdKeyboardDoubleArrowRight } from 'react-icons/md';
 
 const Login = () => {
   const [user, setUser] = useState({
@@ -9,15 +11,15 @@ const Login = () => {
   });
 
   const onChangeInput = (e) => {
-    const {name,value} = e.target;
-    setUser({...user,[name]:value})
+    const { name, value } = e.target;
+    setUser({ ...user, [name]: value })
   }
 
   const loginSubmit = async (e) => {
     e.preventDefault();
-    
+
     try {
-      await axios.post('/user/login',{...user})
+      await axios.post('/user/login', { ...user })
       // console.log({...user})
       console.log(user)
 
@@ -27,8 +29,8 @@ const Login = () => {
 
 
     } catch (error) {
-        alert(error.response.data.msg)
-        console.log(error)
+      alert(error.response.data.msg)
+      console.log(error)
     }
   }
 
@@ -36,20 +38,58 @@ const Login = () => {
   return (
     <>
       <div className='Login mt-[140px]'>
-      Loginnnn
+        {/* Loginnnn */}
 
-        <form onSubmit={loginSubmit} className='flex flex-col gap-2'>
-          <input type='email' value={user.email} name='email' placeholder='Email' required onChange={onChangeInput}/>
-          <input type='password' value={user.password} name='password' placeholder='Password' required onChange={onChangeInput} />
-          <button type='submit'>Login</button>
-        </form>
+        <div className='h-[50vh]  mx-auto flex gap-11 my-7 mt-3 rounded-3xl p-4'>
 
-        <div>
-          New user 
-          <Link to='/signup'>
-          Signup
-          </Link>
+          <div className='basis-[50%] py-3 px-12'>
+
+            <div className='text-2xl font-bold mb-5'>Login</div>
+
+            <form onSubmit={loginSubmit} className='flex flex-col gap-5'>
+              <input type='email' value={user.email} name='email' placeholder='Email' required onChange={onChangeInput} className='p-3 rounded-xl mx-11' />
+              <input type='password' value={user.password} name='password' placeholder='Password' required onChange={onChangeInput} className='p-3 rounded-xl mx-11' />
+              
+              <button type='submit' className='my-6 p-3 rounded-xl mx-11 bg-black text-white font-bold'>Login</button>
+            </form>
+
+            <div className='mx-12 font-semibold flex flex-col'>
+              New user??, do you want to create new account??? 
+              <Link to='/signup' className='text-[#4A69E2] font-bold'>
+                Signup
+              </Link>
+            </div>
+
+          </div>
+
+
+          <div className='bg-white basis-[50%] p-6 px-14 rounded-3xl'>
+
+            <div className='text-3xl font-bold mb-4'>Join CHICKICKS Club Get Rewarded Today.</div>
+
+            <div className='mb-7'>
+              <div>As chickicks club member you get rewarded with what you love for doing what you love.
+                Sign up today and receive immediate access to these Level 1 benefits:
+
+                <ul className='list-disc pl-6 pt-1'> 
+                  <li>Free shipping</li>
+                  <li>A 15% off voucher for your next purchase</li>
+                  <li>Access to members Only products and sales</li>
+                  <li>Special offers, Discounts, promotions and many moree</li>
+                </ul>
+              </div>
+            </div>
+
+            <div className='bg-black text-white p-3 rounded-xl flex justify-between items-center'>
+              JOIN THE CLUB
+              <MdKeyboardDoubleArrowRight color='white' size={20}/>
+            </div>
+
+          </div>
         </div>
+
+
+        <Footer />
 
       </div>
     </>
